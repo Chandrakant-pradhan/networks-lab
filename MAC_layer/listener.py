aa17de424fd87ab2bae64e74a67e571e885ad08e
Chandrakant Pradhan <132835748+Chandrakant-pradhan@users.noreply.github.com>
2024-09-22 19:56:22 +0530

    Add files via upload
    file updated

--- MAC_layer/listener.py
+++ MAC_layer/listener.py
@@ -3,33 +3,39 @@ import pyaudio
 import wave
 import signal
 import time
+import keyboard
 
-#message => frame
+# message => frame
 def frameGenerator(message):
-    #message to frame
-    return message
+    return message  
 
-#input
-MAC = int(input('Enter the device MAC address :'))
-howManyMessage = int(input('Enter how many message to send :'))
+# input
+MAC = int(input('Enter the device MAC address: '))
+howManyMessage = int(input('Enter how many messages to send: '))
+
+# input queue
 InitQueue = []
+SendQueue = []
+
 for i in range(howManyMessage):
-    message = input()
+    message = input(f"Enter message {i + 1}: ")
     frame = frameGenerator(message)
-    InitQueue.append(frame)
+    InitQueue.append([frame, i + 1]) 
 
-def handle_sigint(signal_number, frame):
-    print("Trigger received:", signal_number)
+# Function to handle key events
+def on_key_event(e):
+    if e.name in ['enter', 'return']: 
+        if len(InitQueue) > 0:
+            msg = InitQueue[0][0]  
+            msgIdx = InitQueue[0][1] 
+            SendQueue.append(msg) 
+            InitQueue.pop(0) 
+            print(f"Message {msgIdx} : {msg} added to sending queue")
+        else:
+            print("You sent all the messages")
 
-signal.signal(signal.SIGQUIT, handle_sigint)
+keyboard.on_press(on_key_event)
 
-#run indefinetely
 while True:
-    time.sleep(5)
+    time.sleep(5) 
     print("hi")
-    
-
-
-
-    
-
